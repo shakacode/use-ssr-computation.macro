31955db8ad2bf18e64a51cf94f482ca62105d0c0
Abanoub Ghadban <abanoub@shakacode.com>
2023-07-26 15:43:51 +0300

    add tests for getSSRComputation

--- packages/macro/tests/index.test.ts
+++ packages/macro/tests/index.test.ts
@@ -3,7 +3,7 @@ const plugin = require("babel-plugin-macros");
 
 pluginTester({
   pluginOptions: {
-    useSSRComputation: {
+    SSRComputation: {
       side: "server",
     },
   },
@@ -27,11 +27,36 @@ pluginTester({
 });
 
 pluginTester({
+  pluginOptions: {
+    SSRComputation: {
+      side: "server",
+    },
+  },
+  plugin,
+  snapshot: true,
+  babelOptions: {
+    filename: __filename,
+  },
+  tests: {
+    "no usage": `import { getSSRComputation } from "../lib/get-ssr-computation.macro"`,
+    "server-side": `
+      import { getSSRComputation } from "../lib/get-ssr-computation.macro"
+      const x = getSSRComputation("./a.ssr-computation", []);
+    `,
+    "multiple-imports-server-side": `
+      import { getSSRComputation } from "../lib/get-ssr-computation.macro"
+      const x = getSSRComputation("./a.ssr-computation", [])
+      const y = getSSRComputation("./b.ssr-computation", [])
+    `,
+  },
+});
+
+pluginTester({
   beforeAll: () => {
     process.env['HEEH'] = 'CLIENT_SIDE';
   },
   pluginOptions: {
-    useSSRComputation: {
+    SSRComputation: {
       side: "client",
     },
   },
@@ -55,6 +80,39 @@ pluginTester({
     "custom-wepback-chunk-client-side": `
     import { useSSRComputation } from "../lib/index.macro"
     const x = useSSRComputation("./a.ssr-computation", [], { webpackChunkName: "custom-chunk-name" })
+  `,
+  },
+});
+
+pluginTester({
+  beforeAll: () => {
+    process.env['HEEH'] = 'CLIENT_SIDE';
+  },
+  pluginOptions: {
+    SSRComputation: {
+      side: "client",
+    },
+  },
+
+  plugin,
+  restartTitleNumbering: true,
+  snapshot: true,
+  babelOptions: {
+    filename: __filename,
+  },
+  tests: {
+    "client-side": `
+      import { getSSRComputation } from "../lib/get-ssr-computation.macro"
+      const x = getSSRComputation("./a.ssr-computation", [])
+    `,
+    "multiple-imports-client-side": `
+      import { getSSRComputation } from "../lib/get-ssr-computation.macro"
+      const x = getSSRComputation("./a.ssr-computation", [])
+      const y = getSSRComputation("./b.ssr-computation", [])
+    `,
+    "custom-wepback-chunk-client-side": `
+    import { getSSRComputation } from "../lib/get-ssr-computation.macro"
+    const x = getSSRComputation("./a.ssr-computation", [], { webpackChunkName: "custom-chunk-name" })
   `,
   },
 });
