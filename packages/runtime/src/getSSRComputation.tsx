36683a4340f008de41596a070c2ef3e8972fac80
Abanoub Ghadban <abanoub@shakacode.com>
2023-07-26 18:27:40 +0300

    add function to get from cache or execute

---
+++ packages/runtime/src/getSSRComputation.tsx
@@ -0,0 +1,19 @@
+import { calculateCacheKey, Dependency, parseDependencies } from "./utils";
+
+export default function getSSRComputation(importFn: () => Promise<{ default: (...dependencies: Dependency[]) => any }>, dependencies: any, relativePathToCwd: string) {
+  const cache = window.__SSR_CACHE__;
+  const parsedDependencies = parseDependencies(dependencies);
+
+  // relativePathToCwd is used to make sure that the cache key is unique for each module
+  // and it's not affected by the file that calls it
+  const cacheKey = calculateCacheKey(relativePathToCwd, parsedDependencies);
+  const isCacheHit = cache?.[cacheKey];
+
+  if (isCacheHit) {
+    return Promise.resolve(cache[cacheKey]);
+  }
+
+  return importFn().then((module) => {
+    return module.default(...parsedDependencies);
+  });
+}
--- packages/runtime/src/utils.ts
+++ packages/runtime/src/utils.ts
@@ -4,6 +4,19 @@ export function isDependency(element: any): element is Dependency {
   return typeof element === 'number' || typeof element === 'string' || (typeof element === 'object' && element.uniqueId !== undefined);
 }
 
+export function parseDependencies(dependencies: any): Dependency[] {
+  if (!Array.isArray(dependencies)) {
+    throw new Error('Dependencies must be an array.');
+  }
+
+  return dependencies.map((dependency) => {
+    if (isDependency(dependency)) {
+      return dependency;
+    }
+    throw new Error(`useSSRComputation: dependency ${dependency} is not a valid dependency object`);
+  });
+}
+
 export const calculateCacheKey = (modulePath: string, dependencies: Dependency[]): string => {
   const dependenciesString = dependencies.map((dependency) => {
     if (typeof dependency === 'number') {
