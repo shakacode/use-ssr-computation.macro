0922ad55835432a2c304b7361e053865a1ebdf89
Abanoub Ghadban <abanoub@shakacode.com>
2023-07-28 11:38:07 +0300

    add the ability to ignore caching server-side result

--- packages/runtime/src/useSSRComputation_Server.tsx
+++ packages/runtime/src/useSSRComputation_Server.tsx
@@ -1,5 +1,6 @@
 import { useSSRCache } from "./SSRCacheProvider";
 import { calculateCacheKey } from "./utils";
+import { IgnoreCache } from './utils';
 
 export default function useSSRComputation_Server(fn: (...dependencies: any[]) => any, dependencies: any[], relativePathToCwd: string) {
   const cache = useSSRCache();
@@ -9,12 +10,13 @@ export default function useSSRComputation_Server(fn: (...dependencies: any[]) =>
     // relativePathToCwd is used to make sure that the cache key is unique for each module
     // and it's not affected by the file that calls it
     const cacheKey = calculateCacheKey(relativePathToCwd, dependencies);
-    // check if result is a promise
-    if (result && typeof result.then === 'function') {
-      cache[cacheKey] = result.then(asyncResult => cache[cacheKey] = asyncResult);
-    } else {
-      cache[cacheKey] = result;
+
+    // check if it should ignore caching
+    if (result && result[IgnoreCache]) {
+      return result.result;
     }
+
+    cache[cacheKey] = result;
   }
 
   return result;
