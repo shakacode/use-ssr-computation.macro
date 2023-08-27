a63f8df234fc3ba20addc14a86f7001a5352e32e
Abanoub Ghadban <abanoub@shakacode.com>
2023-08-27 19:08:34 +0300

    make the computation result update synchronously if possible

--- packages/runtime/src/useSSRComputation_Client.tsx
+++ packages/runtime/src/useSSRComputation_Client.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useSSRCache } from "./SSRCacheProvider";
 import { calculateCacheKey, Dependency, Options, parseDependencies } from "./utils";
 
 export default function useSSRComputation_Client(importFn: () => Promise<{ default: (...dependencies: Dependency[]) => any }>, dependencies: any[], options: Options, relativePathToCwd: string) {
+  const [fn, setFn] = useState<(...dependencies: Dependency[])=>any>();
   const [, forceUpdate] = useState(0);
   const cache = useSSRCache();
   const parsedDependencies = parseDependencies(dependencies);
@@ -12,33 +13,47 @@ export default function useSSRComputation_Client(importFn: () => Promise<{ defau
   // and it's not affected by the file that calls it
   const cacheKey = calculateCacheKey(relativePathToCwd, parsedDependencies);
   const isCacheHit = cache?.[cacheKey];
+
   useEffect(() => {
     if (isCacheHit || skip) return;
 
     let isMounted = true;
     importFn().then((module) => {
       if (!isMounted) return;
-      const result = module.default();
-      if (result && typeof result.then === 'function') {
-        result.then(asyncResult => {
-          if (!isMounted) return;
-          cache[cacheKey] = asyncResult;
-          forceUpdate((x) => x + 1);
-        });
-      } else {
-        cache[cacheKey] = result;
-        forceUpdate((x) => x + 1);
-      }
+      // Wrapping to an empty function to avoid calling the function immediately.
+      // https://medium.com/swlh/how-to-store-a-function-with-the-usestate-hook-in-react-8a88dd4eede1
+      setFn(() => module.default);
     });
 
     return () => {
       isMounted = false;
     };
-  }, [isCacheHit, importFn, skip, cacheKey]);
+  }, [isCacheHit, importFn, skip]);
 
-  if (isCacheHit && !skip) {
-    return cache[cacheKey];
+  const result = useMemo(()=> {
+    if (!fn || skip) return null;
+
+    return fn(...parsedDependencies);
+  }, [fn, cacheKey, skip]);
+
+  useEffect(() => {
+    let isMounted = true;
+  
+    if (result && typeof result.then === 'function') {
+      result.then(asyncResult => {
+        if (!isMounted) return;
+        cache[cacheKey] = asyncResult;
+        forceUpdate((x) => x + 1);
+      });
+    }
+  }, [result, cacheKey]);
+
+  if (skip) {
+    return null;
   }
 
-  return null;
+  if (result && typeof result.then !== 'function') {
+    cache[cacheKey] = result;
+  }
+  return cache[cacheKey];
 }
